a40d89030b801a58bcd382d407db43901cc53bd1
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2024-06-30 04:21:11 -0300

    Criado a estrutura inicial do projeto, estruturado pasta componentes, dentro dela, existe as pastas, Autenticação, Escala, Funcionarios, PainelControle. Dentro do diretório src, existe a pasta paginas, contendo PaginaEscala, PaginaFuncionarios, PaginaInicial, PaginaLogin. Atualmente foi implementado dentro do index.js a renderização inicial do componente App com React.StricMode.

---
+++ src/App.js
@@ -0,0 +1,7 @@
+// Importa o React, necessário para renderizar componentes React
+import React from "react";
+
+// Importa componentes do React Router DOM para gerenciar as rotas da aplicação
+import { BrowserRouter as router, Route, Switch } from "react-router-dom";
+
+
