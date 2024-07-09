760920428282e6e1733b7adeace4b9443caa410f
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2024-07-09 20:21:35 -0300

    Login e registro
    Implementado os componentes de login e registro. Ambos compartilham de uma lógica semelhante, onde no registro foi inserido lógicas para a confirmação de senha e verificação de compatibilidade entre os dois campos. Foi inserido um label e input para o terceiro button onde deve ser feita a confirmação de senha, caso as senhas inseridas não sejam as mesmas, uma mensagem será exibida informando que ambas as informações não validão.

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -4,36 +4,50 @@ function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    // função para lidar com alteração  do campo de e-mail
+    const handleChangeEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    // função para lidar com alteração do campo de senha 
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    };
+
+    // função para lidar com o envio do formulário
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Lógica de autenticação aqui
-        console.log('E-mail:', email);
-        console.log('Password:', password);
+
+        // lógica de autenticação 
+        console.log('E-mail', email);
+        console.log('Password', password);
     };
+
+    const isFormValid = email !== '' && password !== '';
 
     return (
         <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="email">E-mail</label>
                 <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required 
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={handleChangeEmail} 
+                    required
                 />
             </div>
-            
+
             <div>
                 <label htmlFor="password">Senha:</label>
                 <input
                     type="password"
                     id="password"
                     value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={handlePassword}
                 />
             </div>
-            <button type="submit">Login</button>
+            <button type="submit" disabled={isFormValid}>Login</button> //botão desativado se os campos não forem válidos
         </form>
     );
 };
---
+++ src/components/Authentication/Register.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+function Register() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    // função para lidar com alteração  do campo de e-mail
+    const handleChangeEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    // função para lidar com alteração do campo de senha 
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    };
+
+    // função que lida com a alteração do campo de confirmação de senha 
+    const handleChangeConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value);
+    }
+
+    // função para lidar com o envio do formulário
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            alert('As senhas não são as mesmas!');
+        }
+
+        // lógica de registro 
+        console.log('E-mail', email);
+        console.log('Password', password);
+    };
+
+    const isFormValid = email !== '' && password !== '' && confirmPassword !== '';
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="email">E-mail</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={handleChangeEmail}
+                    required
+                />
+            </div>
+
+            <div>
+                <label htmlFor="password">Senha:</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={handlePassword}
+                />
+            </div>
+
+            <div>
+                <label htmlFor="confirmPassword">Confirme a senha</label>
+                <input
+                    type="password"
+                    id="confirmPassword"
+                    value={confirmPassword}
+                    onChange={handleChangeConfirmPassword}
+                />
+            </div>
+            <button type="submit" disabled={isFormValid}>Registro</button> //botão desativado se os campos não forem válidos
+        </form>
+    );
+};
+
+export default Register;
