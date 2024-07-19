239e6611d1ae0f897e82cf4c8ed4e8ff77130c74
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2024-07-19 14:13:31 -0300

    estrutura e query's
    Construção do banco de dados, registro de query's e estrutura atualizada para back-end.

---
+++ Back-Node/estrutura/estrutura.mk
@@ -0,0 +1,23 @@
+meu-projeto-backend/
+│
+├── config/
+│   ├── database.js
+│   └── env.js
+│
+├── controllers/
+│   ├── authController.js
+│   └── userController.js
+│
+├── models/
+│   └── User.js
+│
+├── routes/
+│   ├── authRoutes.js
+│   └── index.js
+│
+├── utils/
+│   ├── auth.js
+│   └── validation.js
+│
+├── server.js
+└── package.json
